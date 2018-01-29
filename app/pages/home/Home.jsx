b862552067ee2a23575ba1999948b018a5f2ca64
yang_fy <yang_fy@suixingpay.com>
2018-01-29 15:01:15 +0800

    fix

--- app/pages/home/Home.jsx
+++ app/pages/home/Home.jsx
@@ -1,32 +1,20 @@
 import RouterPager from '../../comps/RouterPager'
 import { connect } from 'react-redux'
 import React from 'react'
-import {setBackGround} from "../../decorators/Background";
 import styles from './Home.scss'
-import fetch from  'sx-fetch'
-import {setWindowScroll} from "../../decorators/WindowScroll";
 @connect(
-    state =>{ return{mixinsScrollState: state.vars.mixinsScrollState} },
+    state =>{ return{} },
     dispatch =>{return{}}
 )
-@setBackGround('#EFEFF4')
-@setWindowScroll()
 export default class Home extends RouterPager {
     constructor (props) {
         super(props)
     }
     componentWillMount(){
-        fetch.get('/users').then(res=>{
-            console.log(res)
-        },err =>{
-            console.log(err)
-        })
     }
     render () {
         return (
-            <div>
-                HOMe
-            </div>
+            <div>Home</div>
         )
     }
 }
