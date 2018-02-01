73f1f560bf9aae55c5535ecc9fa3fa573e2e2345
yang_fy <yang_fy@suixingpay.com>
2018-02-01 11:10:20 +0800

    fix

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ import Home from './pages/home/index'
 import Mine from './pages/mine/index'
 import Protected from './pages/protected/index'
 import Loadable from 'react-loadable'
+import Loadings from './component/Loading.jsx'
 import React from 'react'
 const routes= [
     ...Message,
@@ -10,12 +11,12 @@ const routes= [
     ...Protected,
     ...Mine
 ]
-const Loading =({ isLoading, error }) => {
-    if (isLoading) {
-        return null;
+const Loading =({ error, pastDelay}) => {
+    if (pastDelay) {
+        return <Loadings/>;
     }
     else if (error) {
-        window.location.reload();
+        return <div>页面加载失败。。。请刷新重试</div>
     }
     else {
         return null;
@@ -25,6 +26,7 @@ routes.forEach((value) => {
     value.component  =  Loadable({
         loader: value.component,
         loading: Loading,
+        delay: 300
     });
 })
 export default routes
