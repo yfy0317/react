043205b1240b7e3ec978a2c7e16e15327a5fd39b
yang_fy <yang_fy@suixingpay.com>
2018-03-09 19:47:31 +0800

    fix

--- reactApp.jsx
+++ reactApp.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import 'babel-polyfill'
 import { BrowserRouter, Route} from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './app/redux/store'
